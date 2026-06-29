58daf931860b73ac8d2a11c5a7dd858aa3648dbe
GigiaJ <gigiaboone@yahoo.com>
2026-06-29 17:07:05 -0500

    dump changes

--- src/virtualizer/scroll.cljs
+++ src/virtualizer/scroll.cljs
@@ -1,57 +1,96 @@
 (ns virtualizer.scroll
   (:require [reagent.core :as r]
             [clojure.string :as str]
+            [cljs.core.async :as async :refer [put! chan sliding-buffer close! timeout <! go-loop]]
             [virtualizer.layout :as layout]))
 
 (defn create-scroll-state []
-  {:!anchor                   (atom {:id nil :offset 0 :anchor-bottom 0 :total-height 0})
-   :!current-height           (atom 0)
-   :!current-positioned       (atom [])
-   :!current-focus            (atom false)
-   :!current-was-loading-fwd  (atom false)
-   :!prev-loading-fwd         (atom false)
-   :!dist-bottom              (r/atom 0)
-   :!at-bottom?               (r/atom true)
-   :!show-jump?               (r/atom false)
-   :!initialized?             (r/atom false)
-   :!scroll-timer             (atom nil)})
+  {:!anchor                  (atom {:id nil :offset 0 :anchor-bottom 0 :total-height 0})
+   :!current-height          (atom 0)
+   :!current-positioned      (atom [])
+   :!current-focus           (atom false)
+   :!current-was-loading-fwd (atom false)
+   :!prev-loading-fwd        (atom false)
+   :!dist-bottom             (r/atom 0)
+   :!at-bottom?              (r/atom true)
+   :!show-jump?              (r/atom false)
+   :!initialized?            (r/atom false)
+   :!scroll-top              (atom 0)
+   :!client-height           (atom 800)
+   :scroll-ch                (chan (sliding-buffer 1))})
 
-(defn get-dist [target-el]
-  (let [st    (js/Math.round (.-scrollTop target-el))
-        max-s (- (.-scrollHeight target-el) (.-clientHeight target-el))]
-    (if (<= st 0)
-      (js/Math.abs st)
-      (if (> st (/ max-s 2))
-        (max 0 (- max-s st))
-        0))))
+(defn start-scroll-machine! [scroll-state]
+  (let [ch (:scroll-ch scroll-state)
+        !at-bottom? (:!at-bottom? scroll-state)
+        !show-jump? (:!show-jump? scroll-state)]
+    (go-loop []
+      (when-let [{:keys [ctx dist-from-top dist-from-bottom at-bottom? should-show-jump?]} (<! ch)]
+        (let [{:keys [on-load-older on-load-newer loading-older? older-dead? focus-mode?
+                      loading-newer? chunking? has-items? initialized?]} ctx]
+          (when (and has-items? initialized?)
+            (when (not loading-older?)
+              (when (not= @!at-bottom? at-bottom?) (reset! !at-bottom? at-bottom?)))
+            (when (not= @!show-jump? should-show-jump?) (reset! !show-jump? should-show-jump?))
 
-(defn apply-scroll-anchoring [el state]
-  (let [{:keys [!anchor !current-height !current-positioned !current-focus !current-was-loading-fwd]} state
+            (let [trigger-older? (and (<= dist-from-top 600) (not loading-older?) (not older-dead?) (not chunking?))
+                  trigger-newer? (and focus-mode? (<= dist-from-bottom 600) (not loading-newer?) (not chunking?))]
+              (cond
+                trigger-older?
+                (do
+                  (when on-load-older (on-load-older))
+                  (loop [] (when (async/poll! ch) (recur)))
+                  (<! (timeout 500)))
+
+                trigger-newer?
+                (do
+                  (when on-load-newer (on-load-newer))
+                  (loop [] (when (async/poll! ch) (recur)))
+                  (<! (timeout 500)))))))
+        (recur)))))
+
+
+(defn get-dist
+  "Returns the absolute distance from the logical bottom.
+   In modern flex-direction: column-reverse, scrollTop natively flows <= 0."
+  [st _max-s]
+  (- (js/Math.round st)))
+
+
+
+(defn apply-scroll-anchoring [el state snapshot]
+  (let [{:keys [!anchor !current-height !current-positioned !current-focus
+                !current-was-loading-fwd !scroll-top !client-height]} state
         anchor-val   @!anchor
         total-height @!current-height
+        client-h     @!client-height
         positioned   @!current-positioned
         focus-mode?  @!current-focus
-        was-loading? @!current-was-loading-fwd]
+        was-loading? @!current-was-loading-fwd
+        current-st (if snapshot (:scroll-top snapshot) (.-scrollTop el))
+        max-s      (max 0 (- total-height client-h))]
 
     (when (not= total-height (:total-height anchor-val))
       (let [old-anchor-item (some #(when (= (:id %) (:id anchor-val)) %) positioned)]
         (if old-anchor-item
-          (let [expected-dist (+ (:bottom old-anchor-item) (:offset anchor-val))
-                sync-dist     (get-dist el)
-                sync-err      (- sync-dist expected-dist)]
-            (when-not (<= (js/Math.abs sync-err) 1.5)
-              (when-not (and (<= sync-dist 5) (not was-loading?) (not focus-mode?))
-                (set! (.-scrollTop el) (+ (.-scrollTop el) sync-err)))))
-          (js/console.warn "--> JS ANCHOR LOST: Target Anchor ID unmounted by virtualizer. Re-acquiring..."))))
+          (let [shift (- (:bottom old-anchor-item) (:anchor-bottom anchor-val))]
+            (when-not (zero? shift)
+              (let [sync-dist (get-dist current-st max-s)]
+                (when-not (and (<= sync-dist 5) (not was-loading?) (not focus-mode?))
+                  (let [new-st (- current-st shift)]
+                    (set! (.-scrollTop el) new-st)
+                    (reset! !scroll-top new-st))))))
+          (js/console.warn "--> JS ANCHOR LOST..."))))
 
-    (let [dist-after-shift (get-dist el)
+    (let [final-st         (.-scrollTop el)
+          dist-after-shift (get-dist final-st max-s)
           idx              (layout/binary-search-start-index positioned (inc dist-after-shift))
           valid-anchors    (drop idx positioned)
           new-anchor       (or (first (remove #(let [id (str (:id %))]
-                                                 (or (str/starts-with? id "virtual-")
+                                                 (or (clojure.string/starts-with? id "virtual-")
                                                      (= id "read-marker")))
                                               valid-anchors))
-                               (first valid-anchors))]
+                               (first valid-anchors)
+                               (last positioned))]
       (when new-anchor
         (reset! !anchor
                 {:id            (:id new-anchor)
@@ -59,35 +98,52 @@
                  :anchor-bottom (:bottom new-anchor)
                  :total-height  total-height})))))
 
+
+
 (defn evaluate-scroll-position! [target scroll-state ctx]
-  (let [scroll-top       (js/Math.round (.-scrollTop target))
-        max-scroll       (- (.-scrollHeight target) (.-clientHeight target))
-        dist-from-bottom (if (<= scroll-top 0)
-                           (js/Math.abs scroll-top)
-                           (if (> scroll-top (/ max-scroll 2))
-                             (max 0 (- max-scroll scroll-top))
-                             0))
+  (let [st               (.-scrollTop target)
+        _                (reset! (:!scroll-top scroll-state) st)
+        {:keys [!dist-bottom !anchor !current-height !current-positioned scroll-ch !client-height]} scroll-state
+        ch               @!client-height
+        th               @!current-height
+        max-scroll       (max 0 (- th ch))
+        dist-from-bottom (get-dist st max-scroll)
         dist-from-top    (max 0 (- max-scroll dist-from-bottom))
         at-bottom?       (<= dist-from-bottom 30)
+        should-show-jump? (> dist-from-bottom 600)
 
-        {:keys [!dist-bottom !at-bottom? !scroll-timer !show-jump?]} scroll-state
-        {:keys [on-load-older on-load-newer loading-older? older-dead? focus-mode? loading-newer? has-items? initialized?]} ctx]
+        {:keys [initialized?]} ctx]
 
     (when (not= @!dist-bottom dist-from-bottom)
       (reset! !dist-bottom dist-from-bottom))
 
-    (when (and has-items? (not loading-older?) initialized?)
-      (reset! !at-bottom? at-bottom?)
-      (when @!scroll-timer
-        (js/clearTimeout @!scroll-timer))
-      (reset! !scroll-timer
-              (js/setTimeout
-               (fn []
-                 (reset! !scroll-timer nil)
-                 (reset! !show-jump? (> dist-from-bottom 600))
-                 (when (and (<= dist-from-top 600) (not loading-older?) (not older-dead?))
-                   (when on-load-older (on-load-older)))
-                 (when (and focus-mode? (<= dist-from-bottom 600) (not loading-newer?))
-                   (when on-load-newer (on-load-newer))))
-               ;; need to be more intelligent with the timer here
-               300)))))
+    (when (and initialized? (pos? th))
+      (let [positioned       @!current-positioned
+            dist-after-shift dist-from-bottom
+            idx              (layout/binary-search-start-index positioned (inc dist-after-shift))
+            valid-anchors    (drop idx positioned)
+            new-anchor       (or (first (remove #(let [id (str (:id %))]
+                                                   (or (str/starts-with? id "virtual-")
+                                                       (= id "read-marker")))
+                                                valid-anchors))
+                                 (first valid-anchors)
+                                 (last positioned))]
+        (when new-anchor
+          (reset! !anchor
+                  {:id            (:id new-anchor)
+                   :offset        (- dist-after-shift (:bottom new-anchor))
+                   :anchor-bottom (:bottom new-anchor)
+                   :total-height  th}))))
+
+    (let [{:keys [loading-older? older-dead? chunking? focus-mode? loading-newer?]} ctx
+          threshold       (max 600 (* ch 1.5))
+          trigger-older?  (and (<= dist-from-top threshold) (not loading-older?) (not older-dead?) (not chunking?))
+          trigger-newer?  (and focus-mode? (<= dist-from-bottom threshold) (not loading-newer?) (not chunking?))
+          bottom-changed? (not= @(:!at-bottom? scroll-state) at-bottom?)
+          jump-changed?   (not= @(:!show-jump? scroll-state) should-show-jump?)]
+      (when (or trigger-older? trigger-newer? bottom-changed? jump-changed?)
+        (put! scroll-ch {:ctx ctx
+                         :dist-from-top dist-from-top
+                         :dist-from-bottom dist-from-bottom
+                         :at-bottom? at-bottom?
+                         :should-show-jump? should-show-jump?})))))
